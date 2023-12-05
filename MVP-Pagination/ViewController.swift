4cafe78a909a9680a4732018f972af3218a98e63
Kirilloao <kirilltaraturin747@gmail.com>
2023-12-05 19:11:02 +0100

    Initial Commit

---
+++ MVP-Pagination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP-Pagination
+//
+//  Created by Kirill Taraturin on 05.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
